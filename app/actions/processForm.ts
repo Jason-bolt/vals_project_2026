48a1b81fb058bf0e67f91a35b528400bdef348fc
Jason <appiatujason@gmail.com>
2026-01-29 11:12:35 +0000

    Trimming names

--- app/actions/processForm.ts
+++ app/actions/processForm.ts
@@ -3,12 +3,11 @@
 import { generateCard, Style } from "./generate.card";
 
 export async function processForm(formData: FormData) {
-
   const recipient = formData.get("recipient") as string | undefined;
   const sender = formData.get("sender") as string | undefined;
   const style = formData.get("style") as Style;
-  console.log("recipient", recipient, "style", style);
-  const message = await generateCard(recipient, sender, style);
+  console.log("recipient", recipient?.trim(), "style", style);
+  const message = await generateCard(recipient, sender?.trim(), style);
   console.log(message);
   return message;
 }
